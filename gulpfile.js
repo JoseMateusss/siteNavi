7701dfc8362b80cc28ec573d3235e37d80962db5
José Mateus <you@example.com>
2019-04-21 19:08:37 -0300

    Adaptando projeto para usar o gulp e organizando hierarquia de pastas

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+
+
+//Código do gulp para executar a tarefa do sass (Version 4)
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', gulp.series( function() {
+    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
+    .pipe(sass()) // converter o Sass em CSS
+    .pipe(gulp.dest('src/css'));
+}));
+
+gulp.task('watch', gulp.series( function() {
+    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], gulp.parallel( ['sass'] ));
+}));
+
+gulp.task('default', gulp.series( ['sass', 'watch'] ));
+
+/*
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+gulp.task('sass', function() {
+    return gulp.src(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'])
+    .pipe(sass()) // converter o Sass em CSS
+    .pipe(gulp.dest('src/css'));
+});
+gulp.task('watch', function() {
+    gulp.watch(['node_modules/bootstrap/scss/*.scss', 'src/scss/*.scss'], ['sass'] );
+});
+gulp.task('default', ['sass', 'watch'] );
+*/
